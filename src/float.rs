31549bc827453cd533e3fb5003f1e0a2fe9caa17
Jian Chen <admin@chensoft.com>
2024-01-18 22:55:09 +0800

    float: use macro to avoid conflicts with integer

--- src/float.rs
+++ src/float.rs
@@ -1,23 +1,30 @@
-use ryu::Float;
 use super::Encode;
+use ryu::buffer::Sealed;
 
-impl<T: Float + Sync + Send> Encode for T {
-    fn encode(&self, buf: &mut Vec<u8>) {
-        match !self.is_nonfinite() {
-            true => {
-                buf.reserve(24);
+macro_rules! impl_float {
+    ($t:ident) => {
+        impl Encode for $t {
+            fn encode(&self, buf: &mut Vec<u8>) {
+                match !self.is_nonfinite() {
+                    true => {
+                        buf.reserve(24);
 
-                unsafe {
-                    let len = self.write_to_ryu_buffer(buf.as_mut_ptr().add(buf.len()));
-                    buf.set_len(buf.len() + len);
+                        unsafe {
+                            let len = self.write_to_ryu_buffer(buf.as_mut_ptr().add(buf.len()));
+                            buf.set_len(buf.len() + len);
+                        }
+                    }
+                    false => {
+                        buf.extend_from_slice(self.format_nonfinite().as_bytes());
+                    }
                 }
             }
-            false => {
-                buf.extend_from_slice(self.format_nonfinite().as_bytes());
-            }
         }
-    }
+    };
 }
+
+impl_float!(f32);
+impl_float!(f64);
 
 #[test]
 fn test_encode() {
